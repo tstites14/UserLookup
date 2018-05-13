fce69c4fca43d32fc0577c4bdd798344fba48a4c
Theodore Stites <argionad@gmail.com>
2018-05-13 19:44:33 -0400

    Add fields to User.cs

--- UserLookupBackend/UserLookupBackend/Models/User.cs
+++ UserLookupBackend/UserLookupBackend/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace UserLookupBackend.Models
 {
     public class User
     {
+        [Key] public int id { set; get; }
+
+        public string fname { set; get; }
+        public string lname { set; get; }
+        public string address { set; get; }
+        public string city { set; get; }
+        public string state { set; get; }
+        public int zip { set; get; }
     }
 }
