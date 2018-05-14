91daed04f846924f2099ec7a5163d77c0ec5340d
Theodore Stites <argionad@gmail.com>
2018-05-14 17:09:05 -0400

    Add function to get id# from url

--- js/result.js
+++ js/result.js
@@ -1,11 +1,20 @@
 const getID = () => {
-    //let originalURL;
+    let originalURL = window.top.location.href;
+    console.log(originalURL);
+
+    var regex = new RegExp(`[?&]id(=([^&#]*)|&|#|$)`),
+        result = regex.exec(originalURL);
+
+    if (!result) return null;
+    if (!result[2]) return "";
+
+    return decodeURIComponent(result[2].replace(/\+/g, ""))
 }
 
 $(document).ready(() => {
-    let url = `http://localhost:53077/api/getuser?id=${getID()}`
+    let url = `http://localhost:53077/api/getuser?id=${getID()}`;
+    console.log(url);
     /*$.getJSON(url, function(data) {
         console.log (data);
     });*/
-    console.log(url);
 });
