3298cf42b9648b119d3d9b7d20ca9c1cd2666abe
Theodore Stites <argionad@gmail.com>
2018-05-14 21:03:52 -0400

    Text now updates with data from database

--- js/result.js
+++ js/result.js
@@ -11,10 +11,40 @@ const getID = () => {
     return decodeURIComponent(result[2].replace(/\+/g, ""))
 }
 
-$(document).ready(() => {
-    let url = `http://localhost:53077/api/getuser?id=${getID()}`;
-    console.log(url);
-    /*$.getJSON(url, function(data) {
-        console.log (data);
-    });*/
-});
+async function getData(url) {
+    let jsonInput;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === xhr.DONE) {
+            console.log("Data Loaded");
+            jsonInput = xhr.responseText;
+        } else if (xhr.readyState === xhr.LOADING) {
+            console.log("Loading data...");
+        }
+    };
+    xhr.onload = function() {
+        var output = JSONtoString(jsonInput);
+
+        objectToHTML(output);
+    }
+    xhr.send();
+}
+
+const JSONtoString = (input) => {
+    console.log("Converting raw JSON to object...")
+    return JSON.parse(input);
+}
+
+const objectToHTML = (objectInput) => {
+    document.getElementById("fname").innerHTML = objectInput.fname;
+    document.getElementById("lname").innerHTML = objectInput.lname;
+    document.getElementById("address").innerHTML = objectInput.address;
+    document.getElementById("city").innerHTML = objectInput.city;
+    document.getElementById("state").innerHTML = objectInput.state;
+    document.getElementById("zip").innerHTML = objectInput.zip;
+}
+
+let getUserURL = `http://localhost:53077/api/getuser?id=${getID()}`;
+getData(getUserURL);
